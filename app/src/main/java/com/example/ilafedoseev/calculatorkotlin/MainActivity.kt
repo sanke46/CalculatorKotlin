f14c23774ada35cf344504bb1a9789865edd533d
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-07-03 14:08:06 +0300

    + Correct init private var (Buttons and TextView) + create method to display number + create two setOnClickListener [one and two]

--- app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
@@ -5,34 +5,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toolbar
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var number: TextView
-
     private lateinit var oldNumbers: TextView
 
     //numbers [zero - nine]
-    private val one = findViewById(R.id.one) as Button
-    private val two = findViewById(R.id.two) as Button
-    private val three = findViewById(R.id.three) as Button
-    private val four = findViewById(R.id.four) as Button
-    private val five = findViewById(R.id.five) as Button
-    private val six = findViewById(R.id.six) as Button
-    private val seven = findViewById(R.id.seven) as Button
-    private val eight = findViewById(R.id.eighte) as Button
-    private val nine = findViewById(R.id.nine) as Button
+    private lateinit var one : Button
+    private lateinit var two : Button
+    private lateinit var three : Button
+    private lateinit var four : Button
+    private lateinit var five : Button
+    private lateinit var six : Button
+    private lateinit var seven : Button
+    private lateinit var eight : Button
+    private lateinit var nine : Button
 
     // button actions [AC,+/-,%,-,+,*,/,=,.]
-    private val deleteAll = findViewById(R.id.delete_all) as Button
-    private val plusMinus = findViewById(R.id.plus_minus) as Button
-    private val percent = findViewById(R.id.percent) as Button
-    private val minus = findViewById(R.id.minus) as Button
-    private val plus = findViewById(R.id.plus) as Button
-    private val multiplication = findViewById(R.id.multiplication) as Button
-    private val division = findViewById(R.id.division) as Button
-    private val equals = findViewById(R.id.equals) as Button
-    private val dot = findViewById(R.id.dot) as Button
+    private lateinit var deleteAll : Button
+    private lateinit var plusMinus : Button
+    private lateinit var percent : Button
+    private lateinit var minus : Button
+    private lateinit var plus : Button
+    private lateinit var multiplication : Button
+    private lateinit var division : Button
+    private lateinit var equals : Button
+    private lateinit var dot : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +41,38 @@ class MainActivity : AppCompatActivity() {
         number = findViewById(R.id.number) as TextView
         oldNumbers = findViewById(R.id.old_numbers) as TextView
 
+        one = findViewById(R.id.one) as Button
+        two = findViewById(R.id.two) as Button
+        three = findViewById(R.id.three) as Button
+        four = findViewById(R.id.four) as Button
+        five = findViewById(R.id.five) as Button
+        six = findViewById(R.id.six) as Button
+        seven = findViewById(R.id.seven) as Button
+        eight = findViewById(R.id.eighte) as Button
+        nine = findViewById(R.id.nine) as Button
+
+
+        deleteAll = findViewById(R.id.delete_all) as Button
+        plusMinus = findViewById(R.id.plus_minus) as Button
+        percent = findViewById(R.id.percent) as Button
+        minus = findViewById(R.id.minus) as Button
+        plus = findViewById(R.id.plus) as Button
+        multiplication = findViewById(R.id.multiplication) as Button
+        division = findViewById(R.id.division) as Button
+        equals = findViewById(R.id.division) as Button
+        dot = findViewById(R.id.dot) as Button
+
+        one.setOnClickListener {
+            displayNumber(1)
+            print("number one was pressing")
+        }
+
+        two.setOnClickListener {
+            displayNumber(2)
+            print("number two was pressing")
+        }
     }
+
 
     public fun setNumberToZero(){
         var zero = 0
@@ -49,9 +80,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     //number buttons
-    public fun oneNumber(){
-        var one : Int = 1
-        number.text = this.toString() + one.toString()
+    public fun displayNumber(num : Int){
+        if (number.text.toString().equals("0")) {
+            number.text = num.toString()
+        } else {
+            number.text = number.text.toString() + num.toString()
+        }
     }
 
     //methods buttons actions
