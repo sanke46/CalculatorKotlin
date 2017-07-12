09321c36e6720ea37ab0fe0792ca6db040932bbb
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-07-12 15:15:19 +0300

    + add changeable size numbers + not print dot twice + results to double + with round to 6 symbols + some restyle TextView to big number

--- app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
@@ -14,6 +14,13 @@ import org.jetbrains.anko.toast
 import java.util.*
 import kotlinx.android.synthetic.main.activity_main.number
 import kotlinx.android.synthetic.main.activity_main.old_numbers
+import java.text.DecimalFormat
+import android.util.TypedValue
+import android.widget.HorizontalScrollView
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +31,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val scrollView = findViewById(R.id.scroll) as HorizontalScrollView
+//        scrollView.postDelayed({ scrollView.fullScroll(HorizontalScrollView.FOCUS_RIGHT) }, 10L)
     }
 
     fun actionCalculation() {
@@ -50,6 +60,7 @@ class MainActivity : AppCompatActivity() {
         arrayExample.clear()
         number.text = "0"
         old_numbers.text = "0"
+        testSizeCalc()
     }
 
     fun equalButtonDisplay(view: View) {
@@ -60,26 +71,77 @@ class MainActivity : AppCompatActivity() {
 
         //click equals -> change number to result
         arrayExample.forEach { token ->
-            when {
-                token.equals("-") -> {
-                     arrNumbers = strToResult.split("-")
-                    number.text = "${arrNumbers[0].toInt().minus(arrNumbers[1].toInt())}" }
+            if (arrayExample.contains(".")) {
+                when {
+                    token.equals("-") -> {
+                        arrNumbers = strToResult.split("-")
 
-                token.equals("+") -> {
-                    arrNumbers = strToResult.split("+")
-                    number.text = "${arrNumbers[0].toInt().plus(arrNumbers[1].toInt())}" }
+                        if(arrNumbers[1].length > 6 || arrNumbers[1].length > 6) {
+                            number.text = (Math.round(arrNumbers[0].toDouble().minus(arrNumbers[1].toDouble()) * 10000.0) / 10000.0).toString();
+                        } else {
+                            number.text = "${arrNumbers[0].toDouble().minus(arrNumbers[1].toDouble())}"
+                        }
+                    }
 
-                token.equals("*") -> {
-                    arrNumbers = strToResult.split("*")
-                    number.text = "${arrNumbers[0].toInt().times(arrNumbers[1].toInt())}" }
+                    token.equals("+") -> {
+                        arrNumbers = strToResult.split("+")
 
-                token.equals("/") -> {
-                    arrNumbers = strToResult.split("/")
-                    //divide by zero
-                    if (arrNumbers[1].toInt() == 0) {
-                        old_numbers.text = "Can't devide by 0"
-                    } else {
-                        number.text = "${ (arrNumbers[0].toDouble().div(arrNumbers[1].toDouble()))}"
+                        if(arrNumbers[1].length > 6 || arrNumbers[1].length > 6) {
+                            number.text = (Math.round(arrNumbers[0].toDouble().plus(arrNumbers[1].toDouble()) * 10000.0) / 10000.0).toString();
+                        } else {
+                            number.text = "${arrNumbers[0].toDouble().plus(arrNumbers[1].toDouble())}"
+                        }
+                    }
+
+                    token.equals("*") -> {
+                        arrNumbers = strToResult.split("*")
+
+                        if(arrNumbers[1].length > 6 || arrNumbers[1].length > 6) {
+                            number.text = (Math.round(arrNumbers[0].toDouble().times(arrNumbers[1].toDouble()) * 10000.0) / 10000.0).toString();
+                        } else {
+                            number.text = "${arrNumbers[0].toDouble().times(arrNumbers[1].toDouble())}"
+                        }
+                    }
+
+                    token.equals("/") -> {
+                        arrNumbers = strToResult.split("/")
+                        //divide by zero
+                        if (arrNumbers[1].toInt() == 0) {
+                            old_numbers.text = "Can't devide by 0"
+                        } else {
+                            if(arrNumbers[1].length > 6 || arrNumbers[1].length > 6) {
+                                number.text = (Math.round(arrNumbers[0].toDouble().div(arrNumbers[1].toDouble()) * 10000.0) / 10000.0).toString();
+                            } else {
+                                number.text = "${arrNumbers[0].toDouble().div(arrNumbers[1].toDouble())}"
+                            }
+                        }
+                    }
+                }
+            } else {
+                when {
+                    token.equals("-") -> {
+                        arrNumbers = strToResult.split("-")
+                        number.text = "${arrNumbers[0].toInt().minus(arrNumbers[1].toInt())}"
+                    }
+
+                    token.equals("+") -> {
+                        arrNumbers = strToResult.split("+")
+                        number.text = "${arrNumbers[0].toInt().plus(arrNumbers[1].toInt())}"
+                    }
+
+                    token.equals("*") -> {
+                        arrNumbers = strToResult.split("*")
+                        number.text = "${arrNumbers[0].toInt().times(arrNumbers[1].toInt())}"
+                    }
+
+                    token.equals("/") -> {
+                        arrNumbers = strToResult.split("/")
+                        //divide by zero
+                        if (arrNumbers[1].toInt() == 0) {
+                            old_numbers.text = "Can't devide by 0"
+                        } else {
+                            number.text = "${(arrNumbers[0].toInt().div(arrNumbers[1].toInt()))}"
+                        }
                     }
                 }
             }
@@ -97,10 +159,11 @@ class MainActivity : AppCompatActivity() {
         //display calcButton to "number" view
         var buttonCalc = view as Button
         // add only one symbol
-        if (arrayExample[arrayExample.size-1].equals("-") ||
-                arrayExample[arrayExample.size-1].equals("+") ||
-                arrayExample[arrayExample.size-1].equals("/") ||
-                arrayExample[arrayExample.size-1].equals("*")){
+        if (arrayExample[arrayExample.size - 1].equals("-") ||
+                arrayExample[arrayExample.size - 1].equals("+") ||
+                arrayExample[arrayExample.size - 1].equals("/") ||
+                arrayExample[arrayExample.size - 1].equals("*") ||
+                arrayExample[arrayExample.size - 1].equals(".")){
             return
         } else {
             number.text = number.text.toString().plus(buttonCalc.text.toString())
@@ -110,6 +173,7 @@ class MainActivity : AppCompatActivity() {
         //add calcButton to array
         arrayExample.add(buttonCalc.text.toString())
         println(testArray(arrayExample))
+        testSizeCalc()
     }
 
     fun numberButtonClick(view: View){
@@ -118,7 +182,6 @@ class MainActivity : AppCompatActivity() {
 
 
         //add click number to array
-
             if (number.text.toString().equals("0")) {
                 number.text = buttonNumber.text.toString()
                 arrayExample.add(buttonNumber.text.toString())
@@ -126,6 +189,8 @@ class MainActivity : AppCompatActivity() {
                 number.text = number.text.toString().plus(buttonNumber.text.toString())
                 arrayExample.add(buttonNumber.text.toString())
             }
+
+            testSizeCalc()
         }
 
 
@@ -142,6 +207,20 @@ class MainActivity : AppCompatActivity() {
     fun pressPercent(view: View){
         var procentResult = number.text.toString().toDouble().div(100)
         number.text = procentResult.toString()
+    }
+
+    fun testSizeCalc() {
+        if (number.text.toString().length > 9) {
+           number.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 65F)
+        }
+
+        if (number.text.toString().length > 12) {
+            number.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 55F)
+        }
+
+        if (number.text.toString().length < 1) {
+            number.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 75F)
+        }
     }
 
 
