ae60d8d852d1aabbfca8ad5da4b6a0f3e32992e6
Ilya Fedoseev <shift.of.razum@gmail.com>
2017-07-13 23:26:54 +0300

    Create two methods  "equals"

--- app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ilafedoseev/calculatorkotlin/MainActivity.kt
@@ -13,14 +13,11 @@ import android.widget.TextView
 import android.widget.Toolbar
 import org.jetbrains.anko.toast
 import java.util.*
-import kotlinx.android.synthetic.main.activity_main.number
-import kotlinx.android.synthetic.main.activity_main.old_numbers
 import java.text.DecimalFormat
 import android.util.TypedValue
 import android.widget.HorizontalScrollView
 import android.os.Vibrator
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,13 +28,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val scrollView = findViewById(R.id.scroll) as HorizontalScrollView
-//        scrollView.postDelayed({ scrollView.fullScroll(HorizontalScrollView.FOCUS_RIGHT) }, 10L)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
+
+        // save instance
         number.text = savedInstanceState.getString("number")
         arrayExample = savedInstanceState.getStringArrayList("arrayExample")
     }
@@ -45,7 +41,8 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        outState.putStringArrayList("arrayExample", arrayExample)
+        // save instance
+//        outState.putStringArrayList("arrayExample", arrayExample)
         outState.putString("number", number.text.toString())
     }
 
@@ -60,9 +57,12 @@ class MainActivity : AppCompatActivity() {
 
     fun makeArrayToStr(arr: ArrayList<String>): String {
         var str : StringBuilder = StringBuilder()
-
         for (s in arr) {
-            str.append(s)
+            if(s.equals("-") || s.equals("+") || s.equals("/") || s.equals("*")){
+                str.append(" " + s + " ")
+            } else {
+                str.append(s)
+            }
         }
 
         return str.toString()
@@ -73,12 +73,40 @@ class MainActivity : AppCompatActivity() {
         arrayExample.clear()
         number.text = "0"
         old_numbers.text = "0"
+        tokenList.clear()
         testSizeCalc()
+    }
+
+
+    fun equalButtonDispla3 (view: View) {
+        var currentOp: String = ""
+        var currentNumber = 0
+        var tokenListStr : String = makeArrayToStr(arrayExample)
+        tokenList = tokenListStr.split(" ") as ArrayList<String>
+
+        tokenList.forEach { token ->
+            when {
+                token.equals("+") || token.equals("/") || token.equals("*") || token.equals("-") -> currentOp = token
+                currentOp.equals("-") -> currentNumber -= token.toInt()
+                currentOp.equals("/") -> currentNumber /= token.toInt()
+                currentOp.equals("*") -> currentNumber *= token.toInt()
+                else -> currentNumber += token.toInt()
+
+            }
+            
+        }
+
+        number.text = currentNumber.toString()
+
+        //clean array to empty
+        arrayExample.clear()
+
+        // ann number of rusult to array
+        arrayExample.add(number.text.toString())
     }
 
     fun equalButtonDisplay(view: View) {
         // define what action do
-        var res : Int = 0
         var strToResult : String = makeArrayToStr(arrayExample)
         var arrNumbers : List<String>
 
@@ -94,6 +122,7 @@ class MainActivity : AppCompatActivity() {
                         } else {
                             number.text = "${arrNumbers[0].toDouble().minus(arrNumbers[1].toDouble())}"
                         }
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("+") -> {
@@ -104,6 +133,7 @@ class MainActivity : AppCompatActivity() {
                         } else {
                             number.text = "${arrNumbers[0].toDouble().plus(arrNumbers[1].toDouble())}"
                         }
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("*") -> {
@@ -114,6 +144,7 @@ class MainActivity : AppCompatActivity() {
                         } else {
                             number.text = "${arrNumbers[0].toDouble().times(arrNumbers[1].toDouble())}"
                         }
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("/") -> {
@@ -128,6 +159,7 @@ class MainActivity : AppCompatActivity() {
                                 number.text = "${arrNumbers[0].toDouble().div(arrNumbers[1].toDouble())}"
                             }
                         }
+                        displayOld(arrNumbers[1],token)
                     }
                 }
             } else {
@@ -135,16 +167,19 @@ class MainActivity : AppCompatActivity() {
                     token.equals("-") -> {
                         arrNumbers = strToResult.split("-")
                         number.text = "${arrNumbers[0].toInt().minus(arrNumbers[1].toInt())}"
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("+") -> {
                         arrNumbers = strToResult.split("+")
                         number.text = "${arrNumbers[0].toInt().plus(arrNumbers[1].toInt())}"
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("*") -> {
                         arrNumbers = strToResult.split("*")
                         number.text = "${arrNumbers[0].toInt().times(arrNumbers[1].toInt())}"
+                        displayOld(arrNumbers[1],token)
                     }
 
                     token.equals("/") -> {
@@ -155,10 +190,12 @@ class MainActivity : AppCompatActivity() {
                         } else {
                             number.text = "${(arrNumbers[0].toInt().div(arrNumbers[1].toInt()))}"
                         }
+                        displayOld(arrNumbers[1],token)
                     }
                 }
             }
         }
+
 
         //clean array to empty
         arrayExample.clear()
@@ -187,6 +224,7 @@ class MainActivity : AppCompatActivity() {
         arrayExample.add(buttonCalc.text.toString())
         println(testArray(arrayExample))
         testSizeCalc()
+
     }
 
     fun numberButtonClick(view: View){
@@ -223,7 +261,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun testSizeCalc() {
-        if (number.text.toString().length > 9) {
+        if (number.text.toString().length > 8) {
            number.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 65F)
         }
 
@@ -241,6 +279,14 @@ class MainActivity : AppCompatActivity() {
         // Vibrate for 500 milliseconds
         val vibrationPattern = longArrayOf(0, 15)
         v.vibrate(vibrationPattern, -1);
+    }
+
+    fun displayOld(num : String, token : String) {
+        if(old_numbers.text.toString().equals("0")) {
+            old_numbers.text = makeArrayToStr(arrayExample)
+        } else {
+            old_numbers.text = old_numbers.text.toString().plus("$token$num")
+        }
     }
 
 }
